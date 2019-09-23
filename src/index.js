360a4ef4aa226dabe178b2effab679c447c19d6a
Sergei Belevski <ceperagrey@gmail.com>
2019-09-23 20:18:10 +0300

    Edge tests (green), functionality tests (red)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-function bracketSequence(data) {}
+function bracketSequence(data) {
+    if (!data || typeof data !== 'string') return '';
+}
+
+module.exports = {
+    bracketSequence,
+};
--- test/main.js
+++ test/main.js
@@ -1,3 +1,4 @@
+const {bracketSequence} = require('../src/index.js');
 const expect = require('chai').expect;
 
 const mocks = [
@@ -5,4 +6,26 @@ const mocks = [
         sample: '((}{))()[()]',
         expect: '()[()]',
     },
+    {
+        sample: '(())()[{[[]]}]',
+        expect: '[{[[]]}]',
+    },
 ];
+
+describe('We should firstly test our function for edge cases', () => {
+    it('When empty string given: expecting empty string', () => {
+        expect(bracketSequence('')).to.be.equal('');
+    });
+    it('When not a string given: expecting empty string', () => {
+        expect(bracketSequence()).to.be.equal('');
+        expect(bracketSequence(123)).to.be.equal('');
+    });
+});
+
+describe('Testing common functionality', () => {
+    it('Should give expected results for each of mock cases', () => {
+        mocks.map(mock => {
+            expect(bracketSequence(mock.sample)).to.be.equal(mock.expect);
+        });
+    });
+});
