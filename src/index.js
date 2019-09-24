260660a32f7d43b1520444e7807097691809cd18
Sergei Belevski <ceperagrey@gmail.com>
2019-09-24 12:48:31 +0300

    Небольшой рефакторинг и комментарии

--- src/index.js
+++ src/index.js
@@ -6,15 +6,31 @@ module.exports = {
   bracketReverse,
 };
 
+// Этот словарь пригодится для проверки корректности стека и для реверса скобок
+const bracketPairs = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  ')': '(',
+  ']': '[',
+  '}': '{',
+};
+
+// Финальная функция
 function bracketSequence(data) {
   if (typeof data !== 'string') throw new TypeError('Not a string');
   if (!data) return '';
+  // Делим строку на массив подстрок по признаку наличия некорректных закрывающих скобок
+  // Затем каждую строку из этого массива оборачиваем для проверки в обратную сторону
+  // Таким образом в итоге имеем массив строк с корректными сочетаниями скобок
   let resversedSub = _.compact(
     bracketSplit(data).map(item =>
       bracketReverse(bracketSubst(bracketReverse(item))),
     ),
   );
+  // Определяем длину максимальной строки в этом наборе
   const maxlen = Math.max.apply(null, resversedSub.map(item => item.length));
+  // Отдаем первую попавшуюся строку с максимальной длиной
   return resversedSub.find(item => item.length == maxlen);
 }
 
@@ -22,11 +38,6 @@ function bracketSequence(data) {
 function bracketSubst(data) {
   if (!data) return '';
   const openingBrackets = '({[';
-  const closingBrackets = {
-    ']': '[',
-    ')': '(',
-    '}': '{',
-  };
   let stack = [];
   let ss = '';
   for (let i = 0; i <= data.length - 1; i++) {
@@ -37,11 +48,11 @@ function bracketSubst(data) {
       continue;
     }
     // Если это вообще не скобка - выбрасываем исключение
-    if (!closingBrackets[data.charAt(i)]) {
+    if (!bracketPairs[data.charAt(i)]) {
       throw new TypeError('String includes non-bracket character');
     }
     // Если это закрывающая скобка и на вершине стека находится не соответствующая ей скобка
-    if (stack.pop() != closingBrackets[data.charAt(i)]) {
+    if (stack.pop() != bracketPairs[data.charAt(i)]) {
       return ss;
     }
     // Если это закрывающая скобка и на вершине стека есть соответствующая ей
@@ -50,7 +61,7 @@ function bracketSubst(data) {
   return ss;
 }
 
-// принимает строку, возвращает массив, из последовательностей bracketSubst
+// принимает строку, возвращает массив, из последовательностей строк, разделяемых по первой некорректно закрывающей скобки (рекурсивно)
 function bracketSplit(data) {
   if (!data) return [];
   let currentSegment = bracketSubst(data);
@@ -63,10 +74,9 @@ function bracketSplit(data) {
 
 // Обращает заданную строку со скобками задом наперед
 function bracketReverse(data) {
-  const dict = {'(': ')', '[': ']', '{': '}', ')': '(', ']': '[', '}': '{'};
   return data
     .split('')
     .reverse()
-    .map(s => dict[s])
+    .map(s => bracketPairs[s])
     .join('');
 }
